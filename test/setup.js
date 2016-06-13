3a04046ed7366632bc945249244b10292a8ce1b3
Chris <cpachomski@gmail.com>
2016-06-13 12:26:04 -0400

    install test framework, expectations library, pseudo dom library, and setup test env

---
+++ test/setup.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+
+/*
+	Create testing DOM and apply all the window attributes to the global namespace
+	Put react and expect in global Namespace as well
+ */
+
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+	if (!(key in global)) {
+		global[key] = window[key];
+	}
+});
+
+global.React = react;
+global.expect = expect;
