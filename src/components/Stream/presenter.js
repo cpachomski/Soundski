9e33520aaabb51fc0043e30bee7e4c15aa7e45fd
cpachomski <cpachomski@gmail.com>
2016-06-13 21:40:45 -0400

    add playback redux loop

--- src/components/Stream/presenter.js
+++ src/components/Stream/presenter.js
@@ -1,24 +1,60 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { CLIENT_ID } from '../../constants/auth';
 
-function Stream ({ user, tracks = [], onAuth }) {
-	return (
-		<div>
-			<div>{
-				user ?
-				<div> {user.username} </div> :
-				<button onClick={onAuth} type='button'>Login</button>
-			}
-			</div>
-			<br/>
-			<div className='tracks--list'>
-				{
-					tracks.map((track, i) => {
-						return <div className='track' key={i}>{track.origin.title}</div>
-					})
-				}
-			</div>
-		</div>
-	)
+
+
+
+
+class Stream extends React.Component {
+	componentDidUpdate() {
+		const audioElement = ReactDOM.findDOMNode(this.refs.audio);
+		if(!audioElement) { return; }
+
+		const { activeTrack } = this.props;
+		if ( activeTrack ) {
+			audioElement.play();
+		} else {
+			audoElement.pause();
+		}
+	}
+
+	render() {
+		const { user, tracks = [], activeTrack, onAuth, onPlay } = this.props;
+
+		return (
+				<div>
+					<div>
+						{
+							user ?
+								<div className='username'> { user.username } </div> :
+								<button className='btn login' onClick={onAuth}>Login</button>
+						}
+					</div>
+					<br/>
+					<div>
+						{
+							tracks.map((track, i) => {
+								return (
+									<div className='track' key={i}>
+										{track.origin.title}
+										<button className='btn play' type='button' onClick={() => onPlay(track)}>Play</button>
+									</div>
+								)
+							})
+						}
+					</div>
+					<br/>
+					<div>
+						{
+							activeTrack ?
+							<audio id='audio' ref='audio' src={`${activeTrack.origin.stream_url}?client_id=${CLIENT_ID}`}></audio> :
+							null
+						}
+					</div>
+				</div>
+		)
+	}
 }
 
 export default Stream
